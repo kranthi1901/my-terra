44f635e8ecf744bc2bb0dc1fac2075980e3bae4b
kranthi1901 <kranthi.kokku2002@gmail.com>
2024-03-13 01:03:01 +0530

    association

--- vpc.tf
+++ vpc.tf
@@ -38,3 +38,8 @@ resource "aws_route_table" "my-route" {
     Name = "route"
   }
 }
+#association route table
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.my-web-subnet.id
+  route_table_id = aws_route_table.my-route.id
+}
