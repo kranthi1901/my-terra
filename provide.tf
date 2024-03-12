0b596695b3aa64ace3fd6137dcef1691863f36f7
kranthi1901 <kranthi.kokku2002@gmail.com>
2024-03-13 00:37:47 +0530

    pro

--- provide.tf
+++ provide.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-west-2"
-  access_key = var.my-access-key
-  secret_key = var.my-secret-key
+  access_key = var.my_access_key
+  secret_key = var.my_secret_key
 }
